019a654782f01053048a26af39810d97d8e293c0
TristanF-00 <tfsaypharath@gmail.com>
2025-02-11 18:51:29 -0500

    second commit

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -3,7 +3,12 @@ public class mainTest {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-        System.out.println("This is the project version 1");
+        System.out.println("This is the project version 2");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
 
     }
 }
