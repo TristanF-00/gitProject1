d306876b8121549b5f109cef8254c146728894a3
TristanF-00 <tfsaypharath@gmail.com>
2025-02-11 18:43:30 -0500

    first commit

---
+++ src/test/java/mainTest.java
@@ -0,0 +1,9 @@
+public class mainTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("This is the project version 1");
+
+    }
+}
